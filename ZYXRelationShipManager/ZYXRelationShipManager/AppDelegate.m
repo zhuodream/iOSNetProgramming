cc1a078d616268acf514cd29e73f53ae31204d68
youxin.zhuo <zhuoyouxin@zhuoyouxindeMac-mini.local>
2016-04-28 15:23:11 +0800

    add new file

--- ZYXRelationShipManager/ZYXRelationShipManager/AppDelegate.m
+++ ZYXRelationShipManager/ZYXRelationShipManager/AppDelegate.m
@@ -43,6 +43,25 @@
         }
     }
     
+    NSDictionary *pushNotification = [launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey];
+    if (pushNotification != nil) {
+        NSString *action = [[[pushNotification objectForKey:@"aps"] objectForKey:@"alert"] objectForKey:@"action-loc-key"];
+        ZYXContact *contact = [[ZYXModel sharedModel] contactWithEmailAddress:[pushNotification objectForKey:@"emailAddress"]];
+        
+        // initiate a phone call
+        if ([action isEqualToString:@"Call"]) {
+            NSString *phone = [NSString stringWithFormat:@"tel:%@", contact.phoneNumber];
+            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:phone]];
+            
+            // start an email
+        } else if ([action isEqualToString:@"Email"]) {
+            NSString *email = [NSString stringWithFormat:@"mailto:%@", contact.emailAddress];
+            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:email]];
+            
+        }
+    }
+
+    
 //    BOOL requested = [[NSUserDefaults standardUserDefaults] boolForKey:kPushTokenTransmitted];
 //    if (requested != YES)
 //    {
@@ -142,6 +161,7 @@
     NSLog(@"注册远程通知");
     [application registerForRemoteNotifications];
 }
+
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
 {
     NSString *userId = @"nate@emaildomain.com";
@@ -153,12 +173,80 @@
     
 //    [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kPushTokenTransmitted];
 //    [[NSUserDefaults standardUserDefaults] synchronize];
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSString *postBody = [NSString stringWithFormat:@"user=%@&token=%@", userId, token];
+        
+        NSString *endPoint = @"http://192.168.1.146/chapter10/register.php";
+        NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:endPoint] cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:30.0];
+        request.HTTPMethod = @"POST";
+        request.HTTPBody = [postBody dataUsingEncoding:NSUTF8StringEncoding];
+        [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+        
+        NSError *error = nil;
+        NSHTTPURLResponse *response;
+        
+        [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+        NSLog(@"response = %@", response);
+        if (response.statusCode == 200)
+        {
+            NSLog(@"响应成功");
+        }
+        else
+        {
+            [[[UIAlertView alloc] initWithTitle:@"Error" message:@"Unable to " delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+        }
+    });
 
 }
 
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
 {
     NSLog(@"注册远程通知失败");
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    NSLog(@"收到远程通知");
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSString *action = [[[userInfo objectForKey:@"aps"] objectForKey:@"alert"] objectForKey:@"action-loc-key"];
+        ZYXContact *contact = [[ZYXModel sharedModel] contactWithEmailAddress:[userInfo objectForKey:@"emailAddress"]];
+        
+        NSString *message = [[[userInfo objectForKey:@"aps"] objectForKey:@"alert"] objectForKey:@"body"];
+        if (message == nil)
+        {
+            message = [[userInfo objectForKey:@"aps"] objectForKey:@"alert"];
+        }
+        
+        [UIAlertView alertViewWithTitle:@"Reminder" message:message cancelButtonTitle:@"Cancel" otherButtonTitles:[NSArray arrayWithObjects:@"View Contact", action, nil] onDismiss:^(int buttonIndex) {
+            if (buttonIndex == 0)
+            {
+                ContactsDetailTableViewController *contactVC = [[ContactsDetailTableViewController alloc] initWithStyle:UITableViewStyleGrouped];
+                contactVC.contact = contact;
+                contactVC.presentedModally = YES;
+                
+                UINavigationController *nc = [[UINavigationController alloc] initWithRootViewController:contactVC];
+                
+                [self.navigationController presentViewController:nc animated:YES completion:nil];
+            }
+            else if (buttonIndex == 1)
+            {
+                if ([action isEqualToString:@"Call"])
+                {
+                    NSString *phone = [NSString stringWithFormat:@"tel:%@", contact.phoneNumber];
+                    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:phone]];
+                }
+                else if ([action isEqualToString:@"Email"])
+                {
+                    NSString *email = [NSString stringWithFormat:@"mailto:%@", contact.emailAddress];
+                    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:email]];
+                }
+                [UIApplication sharedApplication].applicationIconBadgeNumber = 0;
+            }
+        } onCancel:^{
+            [UIApplication sharedApplication].applicationIconBadgeNumber = 0;
+        }];
+    });
 }
 
 @end
